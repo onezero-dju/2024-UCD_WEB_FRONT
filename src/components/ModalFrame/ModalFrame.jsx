d5053bed1832ef2bec3b541d82ad4819c3bd45a6
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-09-24 14:55:13 +0900

    feat: useOnClickOutside 훅 생성
    아웃사이드 클릭시 close 기능
     #3

--- src/components/ModalFrame/ModalFrame.jsx
+++ src/components/ModalFrame/ModalFrame.jsx
@@ -1,11 +1,17 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './ModalFrame.css'
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 const ProfileModal = ({
         setModalOpen,
         children
     }) => {
+
     const ref = useRef();
+    
+    useOnClickOutside(ref, () => {
+        setModalOpen(false);
+    })
     
     return (
             <div className='presentation' role="presentation">
@@ -17,12 +23,14 @@ const ProfileModal = ({
                         >
                             X
                         </span>
+                        {/* 아래 임시 이미지 삽입 */}
                         <img
                             className='modal__poster-img'
                             src={`/logo512.png`}
                             alt="modal-img"
                         />
-                        {children}
+                        {/* 아래에 모달에 담길 뷰 컴포넌트 */}
+                        {children} 
                     </div>
                 </div>
             </div>
--- src/hooks/useOnClickOutside.jsx
+++ src/hooks/useOnClickOutside.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const useOnclickOutside = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const useOnclickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+        // console.log("event.target", event.target)
+        if(!ref.current || ref.current.contains(event.target)) {
+            return;
+        }
+        handler();
+    };
+    
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    }
+  }, [ref, handler])
 }
 
 export default useOnclickOutside
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
